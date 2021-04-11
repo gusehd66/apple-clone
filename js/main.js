f48dbeb8a6c37752e98a560583c344d1edccfb8b
gusehd66 <rlagusehd45@naver.com>
2021-04-11 19:04:27 +0900

    section 추가

--- js/main.js
+++ js/main.js
@@ -1,3 +1,34 @@
 window.onload = function () {
+  let bandsSlide = document.querySelector(".bands_slide");
+  let count = 0;
 
+  function next() {
+    if (count < 6) {
+      console.log(event.target.x);
+      ++count;
+    } else {
+      count = 0;
+    }
+    bandsSlide.style.marginLeft = -bandsSlide.children[0].width * count + "px";
+  }
+
+  function prev() {
+    if (count > 0) {
+      console.log(event.target.x);
+      --count;
+    } else {
+      count = 6;
+    }
+    bandsSlide.style.marginLeft = -bandsSlide.children[0].width * count + "px";
+  }
+
+  for (let i = 0; i < bandsSlide.childElementCount; i++) {
+    bandsSlide.children[i].addEventListener("click", function () {
+      if ((event.target.x > 1200) || (event.target.x == 200)) {
+        next();
+      } else {
+        prev();
+      }
+    })
+  }
 }
